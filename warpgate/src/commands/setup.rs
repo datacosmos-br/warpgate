2d99ac820a1e9aa2a528d7c539595a3650143768
marlon-costa-dc <marlon.costa@datacosmos.com.br>
2025-02-22 22:22:55 -0300

    return to canonical as possible

--- warpgate/src/commands/setup.rs
+++ warpgate/src/commands/setup.rs
@@ -361,8 +361,8 @@ pub(crate) async fn command(cli: &crate::Cli) -> Result<()> {
             .paths_relative_to
             .join(&config.store.http.certificate);
         let key_path = config.paths_relative_to.join(&config.store.http.key);
-        std::fs::write(&certificate_path, cert.pem().as_bytes())?;
-        std::fs::write(&key_path, key_pair.serialize_pem().as_bytes())?;
+        std::fs::write(&certificate_path, cert.pem())?;
+        std::fs::write(&key_path, key_pair.serialize_pem())?;
         secure_file(&certificate_path)?;
         secure_file(&key_path)?;
     }
